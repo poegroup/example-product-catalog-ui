63a2fc33904853019c4b4d4d1675336f47c3b5e9
Cameron Bytheway <bytheway.cameron@gmail.com>
2016-12-15 17:18:57 -0700

    update poe-ui

---
+++ README.md
@@ -0,0 +1,16 @@
+# product_catalog_ui
+
+a webapp
+
+## Running
+
+```sh
+git clone https://github.com/cameron/product_catalog_ui
+cd product_catalog_ui
+npm i
+make
+```
+
+## License
+
+MIT
---
+++ src/index.js
@@ -0,0 +1,37 @@
+__webpack_public_path__ = __build_path__;
+/**
+ * Module dependencies
+ */
+
+import Poe from 'poe-ui';
+import {render} from 'react-dom';
+import Forms from 'form-store';
+import Store from 'hyper-store';
+import Client from 'hyper-client-wait1';
+import Format from 'hyper-uri-format';
+import Translate from 'onus-translate';
+import routes from 'onus-router/react?enhancers=basename!../web';
+
+var API_URL = browser.env.API_URL;
+
+/**
+ * Instantiate dependencies
+ */
+
+var format = require('hyper-uri-format')(API_URL);
+var client = new Client(API_URL);
+var store = new Store(client);
+var forms = new Forms(client);
+
+var app = module.exports = Poe({
+  router: {
+    activeLinkClassName: 'is-active',
+    basename: __app_path__,
+    routes: routes
+  },
+  format: format,
+  forms: new Forms(client),
+  store: store,
+  translate: new Translate('.translations.product_catalog_ui')
+});
+render(app, document.getElementById('app'));
